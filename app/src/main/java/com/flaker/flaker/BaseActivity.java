8270ab0830de226465a0e1b16a9c2902c8b65340
Bruce Wong <xenovia@Bruces-MacBook-Pro.local>
2018-02-18 18:38:44 -0800

    remove default toolbar and begin drawer

--- app/src/main/java/com/flaker/flaker/BaseActivity.java
+++ app/src/main/java/com/flaker/flaker/BaseActivity.java
@@ -1,8 +1,12 @@
 package com.flaker.flaker;
 
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Window;
 
 public class BaseActivity extends AppCompatActivity {
 
@@ -10,6 +14,17 @@ public class BaseActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Log.d("whatever", "HELLO WORLD");
+
+        // Used to remove the default top title bar
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//
+//        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+//        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+//                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+//        drawer.addDrawerListener(toggle);
+//        toggle.syncState();
     }
 
 
