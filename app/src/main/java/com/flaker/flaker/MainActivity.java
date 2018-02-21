c1cb12a9386b3934c68b049ee20f5fd185c257e6
Alex Kite <alexkite511@gmail.com>
2018-02-21 11:47:00 -0800

    init commit

--- app/src/main/java/com/flaker/flaker/MainActivity.java
+++ app/src/main/java/com/flaker/flaker/MainActivity.java
@@ -2,22 +2,16 @@ package com.flaker.flaker;
 
 import android.content.pm.PackageManager;
 import android.location.Location;
-import android.nfc.Tag;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.design.widget.NavigationView;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
-import android.util.Log;
-import android.view.View;
-import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
+import android.util.Log;
 import android.view.MenuItem;
 
 import com.google.android.gms.common.api.Status;
@@ -39,7 +33,6 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends BaseActivity {
 
@@ -126,15 +119,21 @@ public class MainActivity extends BaseActivity {
                 int id = item.getItemId();
                 if (id == R.id.nav_camera) {
                     // Handle the camera action
+                    Log.d("1alksdfjaslfd","asldkfjafadsfklj");
                 } else if (id == R.id.nav_gallery) {
+                    Log.d("2alksdfjaslfd","asldkfjafadsfklj");
 
                 } else if (id == R.id.nav_slideshow) {
+                    Log.d("3alksdfjaslfd","asldkfjafadsfklj");
 
                 } else if (id == R.id.nav_manage) {
+                    Log.d("4alksdfjaslfd","asldkfjafadsfklj");
 
                 } else if (id == R.id.nav_share) {
+                    Log.d("5alksdfjaslfd","asldkfjafadsfklj");
 
                 } else if (id == R.id.nav_send) {
+                    Log.d("6alksdfjaslfd","asldkfjafadsfklj");
 
                 }
 
@@ -143,7 +142,19 @@ public class MainActivity extends BaseActivity {
                 return true;
             }
         });
+
+
+
+
+
+
+
+
+
+
     }
+
+
 
     private void setupMapAPIClients() {
         // Construct a GeoDataClient.
